79f02d3c9b7353dc2f43bbbf6ff49beb79907af7
Jesser360 <jesser360@gmail.com>
2017-05-21 11:11:35 -0700

    initial

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var port = process.env.API_PORT || 3001;
 
 //db config
 //ADD YOUR INFO HERE!
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds115738.mlab.com:15738/mern-comment-box');
+mongoose.connect('mongodb://mern-comment-box:123123@ds145790.mlab.com:45790/mern-comment-box');
 
 //config API to use bodyParser and look for JSON in req.body
 app.use(bodyParser.urlencoded({extended: true }));
@@ -32,11 +32,37 @@ app.use(function(req, res, next) {
   res.setHeader('Cache-Control', 'no-cache');
   next();
 });
+app.use('/api', router);
 
 //set route path and init API
 router.get('/', function(req,res) {
   res.json({message: 'API Initialized!'});
 });
+//
+//adding the /comments route to our /api router
+router.route('/comments')
+  .get(function(req, res) {
+    //looks at our Comment Schema
+    Comment.find(function(err, comments) {
+      if (err){
+        res.send(err);
+      }
+      res.json(comments)
+    });
+  })
+  .post(function(req, res) {
+    var comment = new Comment();
+    //body parser lets us use the req.body
+    comment.author = req.body.author;
+    comment.text = req.body.text;
+
+    comment.save(function(err) {
+      if (err){
+        res.send(err);
+      }
+      res.json({ message: 'Comment successfully added!' });
+    });
+  });
 
 // delete all comments
 router.route('/nuke').get(function(req,res){
@@ -45,10 +71,36 @@ router.route('/nuke').get(function(req,res){
   });
 });
 
+
+router.route('/comments/:comment_id')
+.put(function(req,res){
+  Comment.findById(req.params.comment_id, function(err,comment){
+    if(err){
+      res.send(err)
+    }
+    (req.body.author) ? comment.author = req.body.author : "left blank";
+    (req.body.text) ? comment.text = req.body.text : "left blank";
+
+    comment.save(function(err,newComment){
+      if(err){
+        res.send(err, "error")
+      }
+      res.json({message:'comment updated'})
+    })
+  })
+})
+
+.delete(function(req,res){
+  Comment.remove({_id:req.params.comment_id}, function(err, comment){
+    if(err){
+      res.send(err)
+    }
+    res.json({message: "comment deleted"})
+  })
+})
 //add /comments route to our /api router here
 
 //use router config when we call /API
-app.use('/api', router);
 
 //start server
 app.listen(port, function() {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
+import SignUpForm from './SignUpForm';
 
 //more React code here!
+
+ReactDOM.render(
+  <div>
+  <CommentBox url = 'http://localhost:3001/api/comments'
+  pollInterval={2000}/>
+  <SignUpForm/>
+  </div>,
+  document.getElementById('root')
+);
